83e1eeb843491bcd39eef4ad7302590660dd93c2
marcelouls <marcelouls@hotmail.com>
2022-01-24 09:25:46 -0300

    atualizacao sp

---
+++ storage/framework/views/fa43bdc840a0b4414919cffc2024e20a3dd95c58.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Bem me Quero</title>
+
+        <style>
+            body {
+
+                font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+            }
+            h1{
+                text-align: center;
+                margin:20%;
+                font-size: 100px;
+            }
+            img{
+                margin: 20%;
+                height: 50%;
+                width: 50%;
+
+            }
+        </style>
+    </head>
+    <body>
+<div class="container">
+    <div class="col-md-12">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+           <a href="<?php echo e(route('identification')); ?>"> <img src="/img/processed.jpeg" alt="logo"></a>
+        </div>
+        <div class="col-md-3"></div>
+    </div>
+</div>
+    </body>
+    </html>
+
+<?php /**PATH C:\xampp\htdocs\Bemmequero_sp\resources\views/welcome.blade.php ENDPATH**/ ?>
